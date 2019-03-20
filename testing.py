638a6f4ff605155b73173ed4dd01d661686dab01
Eldad Kronfeld <eldadkro@gmail.com>
2019-03-20 22:08:06 +0200

    first one

---
+++ testing.py
@@ -0,0 +1,10 @@
+
+N = int(input())
+if N%2 == 1:
+    print("Weird")
+if N in range(2,5,2):
+    print("Not Weird")
+if N in range(6,22,2):
+    print('Weird')
+if N % 2 == 0 and N >20 :
+    print('Not Weird')
